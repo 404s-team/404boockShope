8fcefd6780665e78d390e4844958ad3d6872219d
ahmed <ahmedsh717@gmail.com>
2020-06-07 13:13:55 +0300

    adding Function.js File

---
+++ js/multipleFunctions.js
@@ -0,0 +1,37 @@
+'user strict';
+Book.all = [];
+function Book(name, author, imgPath, price, type) {
+    this.bookName = name;
+    this.bookauthor = author;
+    this.bookCover = imgPath;
+    this.bookPrice = price;
+    this.bookType = type;
+}[
+    /**hurror */
+    ['The Perfect Wife','Blake Pierce','','','hurror'],
+    ['The Text: A short story','Claire Douglas','','','hurror'],
+    ['And the Sea Became Blood (John Jordan Mysteries Book 20)',' Michael Lister','','','hurror'],
+    ['The Rape Trial','Bidisha Ghosal','','','hurror'],
+    ['A Killer Thriller Collection - Eight The Hard Way (Mystery Thriller Suspense)','Nick Stephenson and Ryan King','','','hurror'],
+    ['The Adventure of the Christmas Pudding: A Hercule Poirot Short Story ','Agatha Christie','','','hurror'],
+    ['The Girl in Room 105','by Chetan Bhagat','','','hurror'],
+    /**History */
+
+    ['A Global History of Architecture','Francis D. K','','','History'],
+    ['World War 1: A History From Beginning to End','Henry Freeman','Chanakya Neeti','','History'],
+    ['Chanakya Neeti','R.P. Jain','','','History'],
+    ['A Brief History of Humankind','Yuval Noah Harari','','','History'],
+    ['The Warren Buffett Way',' Robert Hagstrom','','','History'],
+    /** Fantasi*/
+    ['Harry Potter and the Philosopher','J.K. Rowling','','','Fantasi'],
+    ['The Tales of Beedle the Bard','J.K. Rowling','','','Fantasi'],
+    ['Sita','Amish','','','Fantasi'],
+    ['Jean Angel','Atul Mohite','','','Fantasi'],
+    ['Raavan','Amish Tripathi','','','Fantasi'],
+    /**Kids */
+    ['Computer Security','Dieter Gollmann','','','Kids'],
+    ['Lead Generation for Dummies','Dayna Rothman','','','Kids'],
+    ['Pattern Writing','Wonder House Books','','','Kids'],
+    ['The Blue Umbrella','Ruskin Bond','','','Kids'],
+    ['Grandmas Bag of Stories','Sudha Murty','','','Kids'],
+]
