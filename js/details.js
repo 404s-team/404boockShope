1b09145e6db1625456a33d1fdd50a2c9b891833a
Nedal_Erekat <nedalerekat@gmail.com>
2020-06-08 14:57:21 +0300

    end

--- js/details.js
+++ js/details.js
@@ -1,40 +1,43 @@
 var bookDetails;
-console.log('ALL: '+Book.all);
+addBooks();
+console.log('ALL: ' + Book.all);
 
-function renderDetails(){
-bookDetails = JSON.parse(localStorage.getItem('clickedBook'));
-var bookImg = document.getElementById('choosedImg');
-console.log(bookDetails.bookCover);
-bookImg.setAttribute('src',bookDetails.bookCover);
-var bookTitle = document.getElementById('bookName');
-var bookWriter = document.getElementById('authorName');
-var bookPrice = document.getElementById('price');
-var bookDisc = document.getElementById('bookDisc');
-bookTitle.textContent = bookDetails.bookName;
-bookWriter.textContent = bookDetails.bookauthor;
-bookPrice.textContent = bookDetails.bookPrice;
-bookDisc.textContent = bookDetails.bookDescription;
+
+function renderDetails() {
+    bookDetails = JSON.parse(localStorage.getItem('clickedBook'));
+    var bookImg = document.getElementById('choosedImg');
+    console.log(bookDetails.bookCover);
+    bookImg.setAttribute('src', bookDetails.bookCover);
+    var bookTitle = document.getElementById('bookName');
+    var bookWriter = document.getElementById('authorName');
+    var bookPrice = document.getElementById('price');
+    var bookDisc = document.getElementById('bookDisc');
+    bookTitle.textContent = bookDetails.bookName;
+    bookWriter.textContent = bookDetails.bookauthor;
+    bookPrice.textContent = bookDetails.bookPrice;
+    bookDisc.textContent = bookDetails.bookDescription;
 }
 renderDetails();
 
+
 //Creat recommended books
-var recommendedContainer=document.getElementById('recommended');
+var recommendedContainer = document.getElementById('recommended');
 function renderRecommended() {
-    
-    for(var i=0; i<booksArr.length;i++){
-        var array=booksArr[i];
+
+    for (var i = 0; i < booksArr.length; i++) {
+        var array = booksArr[i];
         console.log(bookDetails.bookType);
-        if(array[4] === bookDetails.bookType){
+        if (array[4] === bookDetails.bookType) {
             console.log(i);
-            
+
             var link = document.createElement('a');
-            link.setAttribute('href','details.html')
+            link.setAttribute('href', 'details.html')
             recommendedContainer.appendChild(link);
             var recommendedImg = document.createElement('img');
             link.appendChild(recommendedImg);
             // recommendedImg.setAttribute('class','imgBestBook');
-            recommendedImg.setAttribute('id',array[6])
-            recommendedImg.setAttribute('src',`${array[2]}`);
+            recommendedImg.setAttribute('id', array[6])
+            recommendedImg.setAttribute('src', `${array[2]}`);
         }
     }
 };
@@ -42,14 +45,15 @@ renderRecommended();
 
 
 recommendedContainer.addEventListener('click', imgClickHandler);
-function imgClickHandler(event){
+function imgClickHandler(event) {
+
     var clickedBook;
-    if (event.target != recommendedContainer){
-        if(event.target.id){
-            console.log('id '+event.target.id);
-        clickedBook = Book.all[event.target.id]
+    if (event.target != recommendedContainer) {
+        if (event.target.id) {
+
+            clickedBook = Book.all[(event.target.id) - 1]
+        }
     }
-}
-console.log( Book.all[event.target.id])
-// localStorage.setItem('clickedBook',JSON.stringify(clickedBook))
+
+    localStorage.setItem('clickedBook', JSON.stringify(clickedBook))
 }
