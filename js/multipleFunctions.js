11754dacf25911f18631d56b1eb85691d373291c
ahmed <ahmedsh717@gmail.com>
2020-06-09 00:25:03 +0300

    adding Form and fix some css stuff and change the books covers

--- js/multipleFunctions.js
+++ js/multipleFunctions.js
@@ -24,7 +24,7 @@ var booksArr = [
     ['World War 1','Henry Freeman','bookCovers/historyBooks/WorldWar1.jpg','18','History','','9'],
     ['Chanakya Neeti','R.P. Jain','bookCovers/historyBooks/ChanakyaNeeti.jpg','5','History','','10'],
     ['A Brief History','Yuval Noah Harari','bookCovers/historyBooks/Sapiens.jpg','70','History','','11'],
-    ['The Warren Buffett Way',' Robert Hagstrom','bookCovers/historyBooks/TheWarrenBuffettWay.jpg','120','History','','12'],
+    ['The Warren Buffett',' Robert Hagstrom','bookCovers/historyBooks/TheWarrenBuffettWay.jpg','120','History','','12'],
     /** Fantasi*/
     ['Harry Potter','J.K. Rowling','bookCovers/scinceBooks/HarryPotter.jpg','7','Fantasi','','13'],
     ['The Tales','J.K. Rowling','bookCovers/scinceBooks/TheTales.jpg','13','Fantasi','','14'],
@@ -32,14 +32,29 @@ var booksArr = [
     ['Jean Angel','Atul Mohite','bookCovers/scinceBooks/JeanAngel.jpg','44','Fantasi','16'],
     ['Raavan','Amish Tripathi','bookCovers/scinceBooks/Raavan.jpg','33','Fantasi','','17'],
     /**Kids */
-    ['Computer Security','Dieter Gollmann','../bookCovers/KidsBooks/ComputerSecurity.jpg','78','Kids','','18'],
-    ['Lead Generation','Dayna Rothman','../bookCovers/KidsBooks/LeadGenerationforDummies.jpg','70','Kids','','19'],
-    ['Pattern Writing','Wonder House Books','../bookCovers/KidsBooks/PatternWriting.jpg','30','Kids','','20'],
-    ['The Blue Umbrella','Ruskin Bond','../bookCovers/KidsBooks/TheBlueUmbrella.jpg','10','Kids','','21'],
-    ['Grandmas Bag','Sudha Murty','../bookCovers/KidsBooks/GrandmasBagofStories.jpg','11','Kids','','22'],
+    // ['Computer Security','Dieter Gollmann','../bookCovers/KidsBooks/ComputerSecurity.jpg','78','Kids','','18'],
+    // ['Lead Generation','Dayna Rothman','../bookCovers/KidsBooks/LeadGenerationforDummies.jpg','70','Kids','','19'],
+    // ['Pattern Writing','Wonder House Books','../bookCovers/KidsBooks/PatternWriting.jpg','30','Kids','','20'],
+    // ['The Blue Umbrella','Ruskin Bond','../bookCovers/KidsBooks/TheBlueUmbrella.jpg','10','Kids','','21'],
+    // ['Grandmas Bag','Sudha Murty','../bookCovers/KidsBooks/GrandmasBagofStories.jpg','11','Kids','','22'],
+
+    ['Learn to Write','Crystal Radke','bookCovers/kidsBooks/myFirstLearntoWrite.jpg','78','Kids','','18'],
+    ['Teach Your Dragon','Steve Herman','bookCovers/kidsBooks/TeachYourDragon.jpg','70','Kids','','19'],
+    ['The Giving Tree','Shel Silverstein','bookCovers/kidsBooks/theGivinTree.jpg','30','Kids','','20'],
+    ['the Places You ll Go!','Dr. Seuss ','bookCovers/kidsBooks/thePlacesYoullGo.jpg','10','Kids','','21'],
+    ['Summer Bridge Activities','unknown','bookCovers/kidsBooks/SummerBridgeActivities.jpg','11','Kids','','22']
+
 ];
+var exchangeableBooks = [
+    ['The Perfect Wife','Blake Pierce','bookCovers/hororBooks/ThePerfectWife.jpg','1','hurror','Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad error, beatae saepe harum eligendi, aperiamvel molestiae laboriosam alias ex quasi ipsa nobis at repellat reiciendis sapiente laborum! Fugit, odio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea delectus blanditiis id error corporis beatae, impedit reprehenderit laudantium nesciunt fuga!','1'],
+    ['The Girl in Room 105','by Chetan Bhagat','bookCovers/hororBooks/TheGirlinRoom105.jpg','31','hurror','','7'],
+    ['A Global History','Francis D. K','bookCovers/historyBooks/AGlobalHistoryofArchitecture.jpg','20','History','','8'],
+    ['Raavan','Amish Tripathi','bookCovers/scinceBooks/Raavan.jpg','33','Fantasi','','17'],
+    ['Learn to Write','Crystal Radke','bookCovers/kidsBooks/myFirstLearntoWrite.jpg','78','Kids','','18']
+]
 var img ;
 var bookTypeVar = 'all';
+var trans = 'download'
 function addBooks(){
     for (let i = 0; i < booksArr.length; i++) {
         var book = new Book(
@@ -53,8 +68,6 @@ function addBooks(){
         );
         Book.all.push(book);
     }
-    var path = String(Book.all[0].bookCover);
-    // console.log('path : ',booksArr[0][2]);
 }
 function mainRender(){
     if (Book.all.length){
