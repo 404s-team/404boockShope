3d16b80067c4a9f2dfaf69ea1f7404ec863b0226
ahmed <ahmedsh717@gmail.com>
2020-06-09 22:32:17 +0300

    animation about us

---
+++ js/aboutUs.js
@@ -0,0 +1,21 @@
+'user strict';
+var img1 = document.getElementById('imgAhmad');
+var img2 = document.getElementById('imgBasma');
+var img3 = document.getElementById('imgIbrahim');
+var img4 = document.getElementById('imgNidal');
+img1.addEventListener('webkitAnimationEnd',function myEndFunction()
+{
+    img1.setAttribute('src','https://scontent.famm7-1.fna.fbcdn.net/v/t1.0-9/s960x960/94794728_3688182014590269_1267899428388208640_o.jpg?_nc_cat=107&_nc_sid=85a577&_nc_eui2=AeEs3dPdVhYHeJYoIQRU8Fr8v2QJyM1BT36_ZAnIzUFPfn-1PBZROpuDeiT3QLb-tudEj6Z1lEO3LzbdXDZXCHOe&_nc_oc=AQkGOjPHZxfFUgVAJv9LFMiTvP06IYgzY9ibaUmX5GhXSW4ET-wFZflLRU-S7humagI&_nc_ht=scontent.famm7-1.fna&_nc_tp=7&oh=adaa6e5cdb01b1318bd76faa8e4acbfd&oe=5F04166B');
+});
+img2.addEventListener('webkitAnimationEnd',function myEndFunction()
+{
+    img2.setAttribute('src','https://avatars2.githubusercontent.com/u/63533086?s=400&v=4');
+});
+img3.addEventListener('webkitAnimationEnd',function myEndFunction()
+{
+    img3.setAttribute('src','https://www.shankarainfra.com/img/avatar.png');
+});
+img4.addEventListener('webkitAnimationEnd',function myEndFunction()
+{
+    img4.setAttribute('src','https://avatars0.githubusercontent.com/u/63533002?s=400&u=d9203ea37176f1525b2ab19b511775601da5cccb&v=4');
+});
