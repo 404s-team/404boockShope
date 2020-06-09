abe5a38a2e34151d905f94ce581031fec1b13fca
Nedal_Erekat <nedalerekat@gmail.com>
2020-06-09 11:17:28 +0300

    update 1

--- js/exForm.js
+++ js/exForm.js
@@ -3,11 +3,15 @@ var btnSubmit = document.getElementById('btnSubmit');
 btnSubmit.addEventListener('click',addExBook);
 function addExBook(e){
     e.preventDefault();
+    trans='exChange';
     var bookName = document.getElementById('BookName').value;
     var bookAuther = document.getElementById('BookName').value;
     var bookDisc = document.getElementById('BookName').value;
     var bookImg = URL.createObjectURL(document.getElementById('imageHolder').files[0]);
     var newExBook = new Book(bookName, bookAuther, bookImg, '0', 'New', bookDisc,'NewBook');
+    // var url = "details.html";
+    // window.location(url);
+    location.replace("details.html")
 
     storExBooks()
     arrExBooksList.push(newExBook);
