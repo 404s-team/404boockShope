d3d5d64621d8e68e0dc938ce4683a6271b84bf4c
ahmed <ahmedsh717@gmail.com>
2020-06-10 17:05:30 +0300

    update new page

--- js/userBook.js
+++ js/userBook.js
@@ -1,17 +1,27 @@
 'user strict';
 var container = document.getElementById('divBooks');
-var books = JSON.parse(localStorage.getItem(''));
+var books = JSON.parse(localStorage.getItem('userBooks'));
 
 function renderUserBooks(){
     for (let i = 0; i < books.length; i++) {
         var divParent = document.createElement('div');
-        divParent.setAttribute('','')
+        divParent.setAttribute('class','divParent')
         container.appendChild(divParent);
         var img = document.createElement('img');
         divParent.appendChild(img);
-        img.setAttribute('')
+        img.setAttribute('class','userBookImg')
+        img.setAttribute('src',books[i].bookCover)
         var divDetails = document.createElement('div');
-        container.appendChild(divDetails);
-        divParent.setAttribute('','')
+        divParent.appendChild(divDetails);
+        divDetails.setAttribute('class','userBookDetails')
+        var bookName = document.createElement('p');
+        var bookAuther = document.createElement('p');
+        divDetails.appendChild(bookName);
+        divDetails.appendChild(bookAuther);
+        bookName.setAttribute('class','userBookName');
+        bookAuther.setAttribute('class','userBookAuther');
+        bookName.textContent = books[i].bookName;
+        bookAuther.textContent = books[i].bookauthor;
     }
 }
+renderUserBooks();
